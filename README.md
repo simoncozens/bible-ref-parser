0bf7c3bf75a4d9501a560eb3446b1f08d4266827
Simon Cozens <simon@simon-cozens.org>
2013-07-22 22:42:40 +0900

    Horrible incompatible change, but you know what, numeric book IDs was always a bad idea.

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ bible-ref-parser
 
 This is a library to help with the tedious job of parsing user-inputted Bible reference strings and turning them into Javascript objects. It deals with most formats that end-users throw at it.
 
-It assumes (erroneously, of course) that all Bibles have the same books with the same names, in the same order, with the same number of chapters and verses. 
-
 Example
 -------
 
@@ -13,22 +11,22 @@ Example
 var b = BibleRefParser("Gen 1:2-12,14; 2; Lev 3:3ff");
 console.log(b.references)
 /*
-   [ { bookId: 1, chapter: 1, startVerse: 2, endVerse: 12, bookOsis: "Gen" },
-     { bookId: 1, chapter: 1, startVerse: 14, endVerse: 14, bookOsis: "Gen" },
-     { bookId: 1, chapter: 2, startVerse: 1, endVerse: 24, bookOsis: "Gen" },
-     { bookId: 3, chapter: 3, startVerse: 3, endVerse: 35, bookOsis: "Lev" }
+   [ { bookId: "Gen", chapter: 1, startVerse: 2, endVerse: 12 },
+     { bookId: "Gen", chapter: 1, startVerse: 14, endVerse: 14 },
+     { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 24 },
+     { bookId: "Lev", chapter: 3, startVerse: 3, endVerse: 35 }
    ],
 */
 
 var iter = b.iterator;
 
-var v = it.next(); /* { bookId: 1, chapter: 1, verse: 2, bookOsis: "Gen" } */
-v = it.next(); /* { bookId: 1, chapter: 1, verse: 3, bookOsis: "Gen" } */
+var v = it.next(); /* { bookId: "Gen", chapter: 1, verse: 2 } */
+v = it.next(); /* { bookId: "Gen", chapter: 1, verse: 3 } */
 /* ... */
 
 ```
 
-`bookOsis` refers to OSIS/SBL normative abbreviations for Bible book names. Generating an `osisRef` ID from a reference entry should be a simple matter of programming.
+The book IDs are taken from OSIS/SBL normative abbreviations for Bible book names. Generating an `osisRef` ID from a reference entry should be a simple matter of programming.
 
 Tests
 -----
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var assert = require("assert");
 test('John 3:16', function(){
   var b = BibleRefParser("John 3:16");
   assert.deepEqual(b.references,
-    [ { bookId: 43, chapter: 3, startVerse: 16, endVerse: 16, bookOsis: "John" },
+    [ { bookId: "John", chapter: 3, startVerse: 16, endVerse: 16 },
     ]
   );
 });
@@ -14,7 +14,7 @@ test('John 3:16', function(){
 test('John 3:16 Junk (non-strict)', function(){
   var b = BibleRefParser("John 3:16 junk at end of reference");
   assert.deepEqual(b.references,
-    [ { bookId: 43, chapter: 3, startVerse: 16, endVerse: 16, bookOsis: "John" },
+    [ { bookId: "John", chapter: 3, startVerse: 16, endVerse: 16 },
     ]
   );
 });
@@ -42,17 +42,17 @@ test('Bad language', function(){
 test('Gen 1:1-2:4', function(){
   var b = BibleRefParser("Gen 1:1-2:4");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 1, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 4, bookOsis: "Gen" } ]
+    [ { bookId: "Gen", chapter: 1, startVerse: 1, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 4 } ]
   );
 });
 
 test('Gen 1:1-2:4; 3:1-3:11', function(){
   var b = BibleRefParser("Gen 1:1-2:4; 3:1-3:11");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 1, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 4, bookOsis: "Gen" },
-      { bookId: 1, chapter: 3, startVerse: 1, endVerse: 11, bookOsis: "Gen" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 1, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 4 },
+      { bookId: "Gen", chapter: 3, startVerse: 1, endVerse: 11 },
    ]
   );
 });
@@ -60,9 +60,9 @@ test('Gen 1:1-2:4; 3:1-3:11', function(){
 test('Gen 1:1-2:4; Lev 3:1-3:11', function(){
   var b = BibleRefParser("Gen 1:1-2:4; Lev 3:1-3:11");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 1, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 4, bookOsis: "Gen" },
-      { bookId: 3, chapter: 3, startVerse: 1, endVerse: 11, bookOsis: "Lev" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 1, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 4 },
+      { bookId: "Lev", chapter: 3, startVerse: 1, endVerse: 11 },
    ]
   );
 });
@@ -70,11 +70,11 @@ test('Gen 1:1-2:4; Lev 3:1-3:11', function(){
 test('Gen 1:1-2:4, 2:14-16; Lev 3:1-3:11; 3:13-16', function(){
   var b = BibleRefParser("Gen 1:1-2:4, 2:14-16; Lev 3:1-3:11; 3:13-16");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 1, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 4, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 14, endVerse: 16, bookOsis: "Gen" },
-      { bookId: 3, chapter: 3, startVerse: 1, endVerse: 11, bookOsis: "Lev" },
-      { bookId: 3, chapter: 3, startVerse: 13, endVerse: 16, bookOsis: "Lev" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 1, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 4 },
+      { bookId: "Gen", chapter: 2, startVerse: 14, endVerse: 16 },
+      { bookId: "Lev", chapter: 3, startVerse: 1, endVerse: 11 },
+      { bookId: "Lev", chapter: 3, startVerse: 13, endVerse: 16 },
    ]
   );
 });
@@ -82,8 +82,8 @@ test('Gen 1:1-2:4, 2:14-16; Lev 3:1-3:11; 3:13-16', function(){
 test('Gen 1:4-,2:3', function(){
   var b = BibleRefParser("Gen 1:4-,2:3");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 4, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 3, endVerse: 3, bookOsis: "Gen" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 4, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 3, endVerse: 3 },
    ]
   );
 });
@@ -91,9 +91,9 @@ test('Gen 1:4-,2:3', function(){
 test('Gen 1:4-; 2 ; 3:3', function(){
   var b = BibleRefParser("Gen 1:4-; 2 ; 3:3");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 4, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 24, bookOsis: "Gen" },
-      { bookId: 1, chapter: 3, startVerse: 3, endVerse: 3, bookOsis: "Gen" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 4, endVerse: 25 },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 24 },
+      { bookId: "Gen", chapter: 3, startVerse: 3, endVerse: 3 },
 
    ]
   );
@@ -103,9 +103,9 @@ test('Gen 1:4-; 2 ; 3:3', function(){
 test('Gen 1:4-, 2 ; 3:3', function(){
   var b = BibleRefParser("Gen 1:4-, 2 ; 3:3");
   assert.deepEqual(b.references,
-    [ { bookId: 1, chapter: 1, startVerse: 4, endVerse: 25, bookOsis: "Gen" },
-      { bookId: 1, chapter: 1, startVerse: 2, endVerse: 2, bookOsis: "Gen" },
-      { bookId: 1, chapter: 3, startVerse: 3, endVerse: 3, bookOsis: "Gen" },
+    [ { bookId: "Gen", chapter: 1, startVerse: 4, endVerse: 25 },
+      { bookId: "Gen", chapter: 1, startVerse: 2, endVerse: 2 },
+      { bookId: "Gen", chapter: 3, startVerse: 3, endVerse: 3 },
 
    ]
   );
@@ -115,13 +115,13 @@ test("Iterator", function(){
   var b = BibleRefParser("Gen 1:2,4,6,8");
   var it = b.iterator();
   var v = it.next();
-  assert.deepEqual(v,{ bookId: 1, chapter: 1, verse: 2, bookOsis: "Gen" });
+  assert.deepEqual(v,{ bookId: "Gen", chapter: 1, verse: 2 });
   v = it.next();
-  assert.deepEqual(v,{ bookId: 1, chapter: 1, verse: 4, bookOsis: "Gen" });
+  assert.deepEqual(v,{ bookId: "Gen", chapter: 1, verse: 4 });
   v = it.next();
-  assert.deepEqual(v,{ bookId: 1, chapter: 1, verse: 6, bookOsis: "Gen" });
+  assert.deepEqual(v,{ bookId: "Gen", chapter: 1, verse: 6 });
   v = it.next();
-  assert.deepEqual(v,{ bookId: 1, chapter: 1, verse: 8, bookOsis: "Gen" });
+  assert.deepEqual(v,{ bookId: "Gen", chapter: 1, verse: 8 });
   v = it.next();
   assert.deepEqual(v, undefined);
 });
@@ -130,7 +130,7 @@ test('Gen 2', function(){
   var b = BibleRefParser("Gen 2");
   assert.deepEqual(b.references,
     [ 
-      { bookId: 1, chapter: 2, startVerse: 1, endVerse: 24, bookOsis: "Gen" },
+      { bookId: "Gen", chapter: 2, startVerse: 1, endVerse: 24 },
    ]
   );
 });
